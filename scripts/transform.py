d783691db0fdcce0f1bf322c361052b2358fd932
Mansi091 <madgulemansi987@gmail.com>
2026-07-24 17:58:37 +0530

    update

--- scripts/transform.py
+++ scripts/transform.py
@@ -4,10 +4,7 @@ print("Loading data...")
 df = pd.read_parquet(r"../data/yellow_tripdata_2026-01.parquet")
 
 print("Cleaning data...")
-# 1. Drop missing values
 df = df.dropna()
 
-# 2. Drop duplicates & reset index to generate a unique trip_id
 df = df.drop_duplicates().reset_index(drop=True)
 df['trip_id'] = df.index
-
