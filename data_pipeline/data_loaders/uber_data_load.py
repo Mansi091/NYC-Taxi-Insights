35a602ed9780bd1bae04a942a57ee02e19002014
Mansi091 <madgulemansi987@gmail.com>
2026-07-24 17:59:15 +0530

    updation 2

--- data_pipeline/data_loaders/uber_data_load.py
+++ data_pipeline/data_loaders/uber_data_load.py
@@ -5,24 +5,12 @@ if 'data_loader' not in globals():
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
-
 @data_loader
 def load_data_from_api(*args, **kwargs):
-    """
-    Loading the Uber data from the local parquet file
-    """
-    # Make sure this path matches exactly where your file is!
-    file_path = r'C:\Users\Mansi\OneDrive\Desktop\Uber\yellow_tripdata_2026-01.parquet'
-    
-    # Read the parquet file into a Pandas DataFrame
+    file_path = r'../data/yellow_tripdata_2026-01.parquet'
     df = pd.read_parquet(file_path)
-    
     return df
-
 
 @test
 def test_output(output, *args) -> None:
-    """
-    Template code for testing the output of the block.
-    """
     assert output is not None, 'The output is undefined'
